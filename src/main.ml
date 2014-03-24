eb0bcc1f3e32cdfc699cc63afe7ff13c33baac34
Ali Assaf <ali.assaf.mail@gmail.com>
2014-03-24 15:42:43 +0100

    Add a version display.

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,11 @@
 (* Parse the command line arguments and execute the given file. *)
 
+let version = "1.2"
+
+let print_version () =
+  Format.printf "Camelide version %s@." version;
+  exit 0
+
 let files = ref []
 
 let add_file filename =
@@ -12,7 +18,9 @@ let options = Arg.align [
   "-", Arg.Unit(fun () -> add_file "-"), " Read from stdin instead of a file";
   "--coc", Arg.Set(Type.use_coc), " Use the full Calculus of Construction modulo (experimental)";
   "--nocheckdep", Arg.Clear(Module.check_dependencies), " Load dependencies without checking (unsafe)";
-  "-v", Arg.Set_int(Error.verbose_level), "<level> Set verbosity level" ]
+  "-v", Arg.Set_int(Error.verbose_level), "<level> Set verbosity level";
+  "-version", Arg.Unit(print_version), " Print version and exit";
+  ]
 
 let argument_error () =
   Arg.usage options usage;
